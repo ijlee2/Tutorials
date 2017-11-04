f9c12314badc9f642250bf1a303378f395799f48
ijlee2 <ijlee2@users.noreply.github.com>
2017-11-04 16:57:21 -0500

    added tests

--- ember-official/tests/acceptance/list-rentals-test.js
+++ ember-official/tests/acceptance/list-rentals-test.js
@@ -3,33 +3,50 @@ import moduleForAcceptance from "ember-official/tests/helpers/module-for-accepta
 
 moduleForAcceptance("Acceptance | list rentals");
 
-// test("visiting /", function(assert) {
-//     visit("/");
-
-//     andThen(function() {
-//         assert.equal(currentURL(), "/");
-//     });
-// });
 
 test("should show rentals as the home page", function(assert) {
+    // Load the given URL
+    visit("/");
 
+    // Wait until the previous commands have been run
+    andThen(function() {
+        assert.equal(currentURL(), "/rentals", "should redirect to rentals page");
+    });
 });
+
 
 test("should link to information about the company", function(assert) {
+    visit("/");
 
+    // Simulate a mouse click
+    click(`a:contains("About")`);
+
+    andThen(function() {
+        assert.equal(currentURL(), "/about", "should redirect to about us page");
+    });
 });
+
 
 test("should link to contact information", function(assert) {
+    visit("/");
 
+    click(`a:contains("Contact")`);
+
+    andThen(function() {
+        assert.equal(currentURL(), "/contact", "should redirect to contact us page");
+    });
 });
+
 
 test("should list available rentals", function(assert) {
 
 });
 
+
 test("should filter the list of rentals by city", function(assert) {
 
 });
+
 
 test("should show details for a selected rental", function(assert) {
 
