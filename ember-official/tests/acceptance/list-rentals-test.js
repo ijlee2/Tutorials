88155b6f9d450c7cffd0d4ca4e711b16f11e1137
ijlee2 <ijlee2@users.noreply.github.com>
2017-11-04 16:29:59 -0500

    initial build

---
+++ ember-official/tests/acceptance/list-rentals-test.js
@@ -0,0 +1,36 @@
+import { test } from "qunit";
+import moduleForAcceptance from "ember-official/tests/helpers/module-for-acceptance";
+
+moduleForAcceptance("Acceptance | list rentals");
+
+// test("visiting /", function(assert) {
+//     visit("/");
+
+//     andThen(function() {
+//         assert.equal(currentURL(), "/");
+//     });
+// });
+
+test("should show rentals as the home page", function(assert) {
+
+});
+
+test("should link to information about the company", function(assert) {
+
+});
+
+test("should link to contact information", function(assert) {
+
+});
+
+test("should list available rentals", function(assert) {
+
+});
+
+test("should filter the list of rentals by city", function(assert) {
+
+});
+
+test("should show details for a selected rental", function(assert) {
+
+});
