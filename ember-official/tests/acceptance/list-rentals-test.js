a5ec4c514be1bfc89c7b32ef75991aabfbf5f11c
ijlee2 <ijlee2@users.noreply.github.com>
2017-11-05 08:42:25 -0600

    data added

--- ember-official/tests/acceptance/list-rentals-test.js
+++ ember-official/tests/acceptance/list-rentals-test.js
@@ -39,7 +39,11 @@ test("should link to contact information", function(assert) {
 
 
 test("should list available rentals", function(assert) {
+    visit("/");
 
+    andThen(function() {
+        assert.equal(find(".listing").length, 3, "should see 3 listings");
+    });
 });
 
 
