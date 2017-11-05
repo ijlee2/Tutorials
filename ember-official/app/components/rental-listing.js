c912a10eff7124f25165cc80d0d4b2c27825adde
ijlee2 <ijlee2@users.noreply.github.com>
2017-11-05 09:08:46 -0600

    added rental listing component

---
+++ ember-official/app/components/rental-listing.js
@@ -0,0 +1,11 @@
+import Component from "@ember/component";
+
+export default Component.extend({
+    "isWide": false,
+
+    "actions": {
+        toggleImageSize() {
+            this.toggleProperty("isWide");
+        }
+    }
+});
---
+++ ember-official/tests/integration/components/rental-listing-test.js
@@ -0,0 +1,57 @@
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
+import EmberObject from "@ember/object";
+import { run } from "@ember/runloop";
+
+// Test object
+const rental = EmberObject.create({
+    "image"       : "fake.png",
+    "title"       : "test-title",
+    "owner"       : "test-owner",
+    "propertyType": "test-type",
+    "city"        : "test-city",
+    "bedrooms"    : 3
+});
+
+moduleForComponent("rental-listing", "Integration | Component | rental listing", {
+    "integration": true
+});
+
+test("should display rental details", function(assert) {
+    this.set("rentalObj", rental);
+    this.render(hbs`{{rental-listing rental=rentalObj}}`);
+
+    assert.equal(this.$(".listing h3").text(), "test-title", "Title: test-title");
+    assert.equal(this.$(".listing .owner").text().trim(), "Owner: test-owner", "Owner: test-owner");
+});
+
+test("should toggle wide class on click", function(assert) {
+    this.set("rentalObj", rental);
+    this.render(hbs`{{rental-listing rental=rentalObj}}`);
+
+    assert.equal(this.$(".image.wide").length, 0, "initially rendered small");
+    run(() => document.querySelector(".image").click());
+
+    assert.equal(this.$(".image.wide").length, 1, "rendered wide after click");
+    run(() => document.querySelector(".image").click());
+    
+    assert.equal(this.$(".image.wide").length, 0, "rendered small after second click");
+});
+
+// test("it renders", function(assert) {
+//     // Set any properties with this.set("myProperty", "value");
+//     // Handle any actions with this.on("myAction", function(val) { ... });
+
+//     this.render(hbs`{{rental-listing}}`);
+
+//     assert.equal(this.$().text().trim(), "");
+
+//     // Template block usage:
+//     this.render(hbs`
+//         {{#rental-listing}}
+//             template block text
+//         {{/rental-listing}}
+//     `);
+
+//     assert.equal(this.$().text().trim(), "template block text");
+// });
