059ba72ab58b0a483be8f31adb3390ca24e60e34
ijlee2 <ijlee2@users.noreply.github.com>
2017-11-05 09:56:15 -0600

    search added

--- ember-official/tests/acceptance/list-rentals-test.js
+++ ember-official/tests/acceptance/list-rentals-test.js
@@ -48,7 +48,15 @@ test("should list available rentals", function(assert) {
 
 
 test("should filter the list of rentals by city", function(assert) {
+    visit("/");
 
+    fillIn(".list-filter input", "Seattle");
+    keyEvent(".list-filter input", "keyup", 69);
+
+    andThen(function() {
+        assert.equal(find(".listing").length, 1, "should show 1 listing");
+        assert.equal(find(`.listing .location:contains("Seattle")`).length, 1, "should contain 1 listing with location Seattle");
+    });
 });
 
 
--- ember-official/tests/integration/components/list-filter-test.js
+++ ember-official/tests/integration/components/list-filter-test.js
@@ -1,24 +1,83 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
+import wait from "ember-test-helpers/wait";
+import RSVP from "rsvp";
 
-moduleForComponent('list-filter', 'Integration | Component | list filter', {
-  integration: true
+const ITEMS = [
+    {"city": "San Francisco"},
+    {"city": "Portland"},
+    {"city": "Seattle"}
+];
+
+const FILTERED_ITEMS = [
+    {"city": "San Francisco"}
+];
+
+moduleForComponent("list-filter", "Integration | Component | list filter", {
+    "integration": true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test("should initially load all listings", function(assert) {
+    // We return promises, since data may be fetched asynchronously
+    this.on("filterByCity", () => {
+        return RSVP.resolve({"results": ITEMS});
+    });
 
-  this.render(hbs`{{list-filter}}`);
+    this.render(hbs`
+        {{#list-filter filter=(action "filterByCity") as |results|}}
+            <ul>
+                {{#each results as |item|}}
+                    <li class="city">{{item.city}}</li>
+                {{/each}}
+            </ul>
+        {{/list-filter}}
+    `);
 
-  assert.equal(this.$().text().trim(), '');
+    return wait().then(() => {
+        assert.equal(this.$(".city").length, 3);
+        assert.equal(this.$(".city").first().text().trim(), "San Francisco");
 
-  // Template block usage:
-  this.render(hbs`
-    {{#list-filter}}
-      template block text
-    {{/list-filter}}
-  `);
+    });
 
-  assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test("should update with matching listings", function(assert) {
+    this.on("filterByCity", value => {
+        if (value === "") {
+            return RSVP.resolve({
+                "query"  : value,
+                "results": ITEMS
+            });
+
+        } else {
+            return RSVP.resolve({
+                "query"  : value,
+                "results": FILTERED_ITEMS
+            });
+
+        }
+
+    });
+
+    this.render(hbs`
+        {{#list-filter filter=(action "filterByCity") as |results|}}
+            <ul>
+            {{#each results as |item|}}
+                <li class="city">
+                    {{item.city}}
+                </li>
+            {{/each}}
+            </ul>
+        {{/list-filter}}
+    `);
+
+    // Simulate filtering results
+    this.$(".list-filter input").val("San").keyup();
+
+    return wait().then(() => {
+        assert.equal(this.$(".city").length, 1);
+        assert.equal(this.$(".city").text().trim(), "San Francisco");
+
+    });
+
 });
