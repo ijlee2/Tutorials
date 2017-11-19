ce57abc1f2b673b9265aecfee098feb5cf88a28a
ijlee2 <ijlee2@users.noreply.github.com>
2017-11-19 13:44:09 -0600

    added library index and new pages

---
+++ ember-zoltan/app/routes/libraries/new.js
@@ -0,0 +1,22 @@
+import Route from "@ember/routing/route";
+
+export default Route.extend({
+    model() {
+        return this.store.createRecord("library");
+    },
+
+    "actions": {
+        saveLibrary(library) {
+            library.save().then(() => this.transitionTo("libraries"));
+        },
+
+        // willTransition is called when the user leaves the page (route)
+        // We use this to reset the model if the model doesn't get saved
+        // to the database
+        willTransition() {
+            // rollbackAttributes removes the record from the store
+            // if the model is new
+            this.controller.get("model").rollbackAttributes();
+        }
+    }
+});
