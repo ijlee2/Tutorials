88812f580072da37f460812111c607f718dc881e
ijlee2 <ijlee2@users.noreply.github.com>
2017-11-19 13:13:04 -0600

    added libraries page

--- ember-zoltan/app/router.js
+++ ember-zoltan/app/router.js
@@ -1,18 +1,22 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config      from "./config/environment";
 
 const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+    "location": config.locationType,
+    "rootURL" : config.rootURL
 });
 
 Router.map(function() {
-  this.route('about');
-  this.route('contact');
+    this.route("about");
+    this.route("contact");
 
-  this.route('admin', function() {
-    this.route('invitations');
-  });
+    this.route("libraries", function() {
+        this.route("new");
+    });
+
+    this.route("admin", function() {
+        this.route("invitations");
+    });
 });
 
 export default Router;
