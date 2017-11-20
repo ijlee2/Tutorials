9255b3c0c2b98b0eeb981b2cf87870de89097b9e
ijlee2 <ijlee2@users.noreply.github.com>
2017-11-20 10:23:57 -0600

    added delete to admin pages

--- ember-zoltan/app/routes/admin/contacts.js
+++ ember-zoltan/app/routes/admin/contacts.js
@@ -3,5 +3,11 @@ import Route from "@ember/routing/route";
 export default Route.extend({
     model() {
         return this.store.findAll("contact");
+    },
+
+    "actions": {
+        deleteContact(contact) {
+            contact.destroyRecord();
+        }
     }
 });
--- ember-zoltan/app/routes/admin/invitations.js
+++ ember-zoltan/app/routes/admin/invitations.js
@@ -3,5 +3,11 @@ import Route from "@ember/routing/route";
 export default Route.extend({
     model() {
         return this.store.findAll("invitation");
+    },
+
+    "actions": {
+        deleteInvitation(invitation) {
+            invitation.destroyRecord();
+        }
     }
 });
