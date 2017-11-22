e18f2f604756a72b9b5f0f270b95da54384b66ff
ijlee2 <ijlee2@users.noreply.github.com>
2017-11-22 10:49:54 -0600

    refactored library components

---
+++ ember-zoltan/app/components/library-item-form.js
@@ -0,0 +1,11 @@
+import Component from "@ember/component";
+
+export default Component.extend({
+    "buttonLabel": "Save",
+
+    "actions": {
+        buttonClicked(param) {
+            this.sendAction("action", param);
+        }
+    }
+});
