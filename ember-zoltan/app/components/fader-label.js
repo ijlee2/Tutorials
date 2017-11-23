8c9505e4ee9a3b46660a510c237057e7070d7a84
ijlee2 <ijlee2@users.noreply.github.com>
2017-11-23 18:56:57 -0500

    added seeder-block and fader-label

---
+++ ember-zoltan/app/components/fader-label.js
@@ -0,0 +1,30 @@
+import Ember     from "ember";
+import Component from "@ember/component";
+
+export default Component.extend({
+    "tagName": "span",
+
+    "classNames"       : ["badge", "badge-success", "badge-fade"],
+    "classNameBindings": ["isShowing:badge-show"],
+
+    "isShowing": false,
+
+    "isShowingChanged": Ember.observer("isShowing", function() {
+        // Users could navigate away from this page in less than 3 seconds. This
+        // component will be destroyed, however our setTimeout task tries to run.
+        // We save this task to a local variable so that it can be cleaned up
+        // during the destroy process. Otherwise, we will see a "calling set on
+        // destroyed object" error.
+        this._runLater = Ember.run.later(() => this.set("isShowing", false), 3000);
+    }),
+
+    resetRunLater() {
+        this.set("isShowing", false);
+        Ember.run.cancel(this._runLater);
+    },
+
+    willDestroy() {
+        this.resetRunLater();
+        this._super(...arguments);
+    }
+});
---
+++ ember-zoltan/app/components/seeder-block.js
@@ -0,0 +1,34 @@
+import Ember     from "ember";
+import Component from "@ember/component";
+
+const MAX_VALUE = 100;
+
+export default Component.extend({
+    "counter": null,
+
+    "isCounterValid"   : Ember.computed.lte("counter", MAX_VALUE),
+    "isCounterNotValid": Ember.computed.not("isCounterValid"),
+    "placeholder"      : `Max ${MAX_VALUE}`,
+
+    "generateReady": false,
+    "deleteReady"  : false,
+
+    "generateInProgress": false,
+    "deleteInProgress"  : false,
+
+    "generateIsDisabled": Ember.computed.or("isCounterNotValid", "generateInProgress", "deleteInProgress"),
+    "deleteIsDisabled"  : Ember.computed.or("generateInProgress", "deleteInProgress"),
+
+    "actions": {
+        generateAction() {
+            if (this.get("isCounterValid")) {
+                // Action up to Seeder Controller with the requested amount
+                this.sendAction("generateAction", this.get("counter"));
+            }
+        },
+
+        deleteAction() {
+            this.sendAction("deleteAction");
+        }
+    }
+});
