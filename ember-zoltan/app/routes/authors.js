318836bf4d3226f51b377258dc1934f1a03103fe
ijlee2 <ijlee2@users.noreply.github.com>
2017-11-23 21:21:02 -0500

    edit authors added

--- ember-zoltan/app/routes/authors.js
+++ ember-zoltan/app/routes/authors.js
@@ -3,5 +3,25 @@ import Route from "@ember/routing/route";
 export default Route.extend({
     model() {
         return this.store.findAll("author");
+    },
+
+    "actions": {
+        editAuthor(author) {
+            author.set("isEditing", true);
+        },
+
+        cancelEditAuthor(author) {
+            author.set("isEditing", false);
+            author.rollbackAttributes();
+        },
+
+        saveAuthor(author) {
+            if (author.get("isNameNotValid")) {
+                return;
+            }
+
+            author.set("isEditing", false);
+            author.save();
+        }
     }
 });
