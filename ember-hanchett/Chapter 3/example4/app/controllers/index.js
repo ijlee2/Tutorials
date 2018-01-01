e295b96697f935fca5809c7609bd848971c1f542
ijlee2 <ijlee2@users.noreply.github.com>
2017-12-31 20:38:59 -0600

    Added Chapter 3, Example 4

--- ember-hanchett/Chapter 3/example4/app/controllers/index.js
+++
@@ -1,8 +0,0 @@
-// app/controllers/index.js
-import Ember from 'ember';
-export default Ember.Controller.extend({
-  url: 'http://placehold.it/350x200',
-  sideClass:'cc',
-  secondClass: 'dd'
-});
-
