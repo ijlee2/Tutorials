db6f6325633ae4e3d168c41e6b872a611d9ff4e1
ijlee2 <ijlee2@users.noreply.github.com>
2017-12-31 20:34:37 -0600

    Added Chapter 3, Example 3

---
+++ ember-hanchett/Chapter 3/example03/app/controllers/student.js
@@ -0,0 +1,15 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+    students: [
+        {
+            name: "Erik"
+        },
+        {
+            name: "Jim"
+        },
+        {
+            name: "Jane"
+        }
+    ]
+});
--- ember-hanchett/Chapter 3/example3/app/controllers/student.js
+++
@@ -1,5 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-	students: [ {name: 'Erik'}, {name: 'Jim'}, {name: 'Jane'}]
-});
