243f43ec684192d3fb19e2c42cce664e4ae3398b
ijlee2 <ijlee2@users.noreply.github.com>
2017-12-31 21:10:51 -0600

    Added Chapter 3, Example 5

---
+++ ember-hanchett/Chapter 3/example05/app/router.js
@@ -0,0 +1,15 @@
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
+
+const Router = EmberRouter.extend({
+    location: config.locationType,
+    rootURL: config.rootURL
+});
+
+Router.map(function() {
+    this.route("students", {
+        path: "/students/:student_id"
+    });
+});
+
+export default Router;
