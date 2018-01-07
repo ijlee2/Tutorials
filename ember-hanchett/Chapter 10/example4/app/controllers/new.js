3320c886037be6a17d9186eaed2d5d295f166df6
ijlee2 <ijlee2@users.noreply.github.com>
2018-01-07 16:50:07 -0600

    Added Chapter 10

--- ember-hanchett/Chapter 10/example4/app/controllers/new.js
+++ ember-hanchett/Chapter 10/example4/app/controllers/new.js
@@ -1,4 +1,23 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
+import cleanURI from "../utils/clean";
 
 export default Controller.extend({
+    actions: {
+        save() {
+            const titleURL = cleanURI(this.get("title"));
+            
+            const post = this.store.createRecord("post", {
+                title: this.get("title"),
+                body: this.get("body"),
+                author: "Me!",
+                titleURL
+            });
+            post.save();
+
+            this.set("title", "");
+            this.set("body", "");
+
+            this.transitionToRoute("index");
+        }
+    }
 });
--- ember-hanchett/Chapter 10/example4/app/controllers/posts.js
+++ ember-hanchett/Chapter 10/example4/app/controllers/posts.js
@@ -1,4 +1,35 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
+import cleanURI from "../utils/clean";
 
 export default Controller.extend({
+    actions: {
+        edit() {
+            this.set("isEditing", true);
+        },
+
+        delete() {
+            this.get("model").forEach(model => {
+                model.deleteRecord();
+            });
+
+            this.get("model").save();
+            this.set("isEditing", false);
+
+            this.transitionToRoute("index");
+        },
+
+        save() {
+            this.get("model").forEach(model => {
+                const titleURL = cleanURI(model.get("title"));
+
+                model.set("titleURL", titleURL);
+
+                model.save();
+            });
+
+            this.set("isEditing", false);
+
+            this.transitionToRoute("index");
+        }
+    }
 });
