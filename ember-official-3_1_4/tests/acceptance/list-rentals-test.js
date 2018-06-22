d2b7760ebc4bbc0669a8b3c901f9e4f90f8870c6
ijlee2 <ijlee2@users.noreply.github.com>
2018-06-21 22:45:27 -0500

    Added routes and templates

---
+++ ember-official-3_1_4/tests/acceptance/list-rentals-test.js
@@ -0,0 +1,33 @@
+import { module, test } from 'qunit';
+import { visit, click, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | list rentals', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('should show rentals as the home page', async function (assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+  });
+
+  test('should link to information about the company.', async function (assert) {
+    await visit('/');
+    await click('.menu-about');
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
+
+  test('should link to contact information.', async function (assert) {
+    await visit('/');
+    await click('.menu-contact');
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
+
+  test('should list available rentals.', async function (assert) {
+  });
+
+  test('should filter the list of rentals by city.', async function (assert) {
+  });
+
+  test('should show details for a selected rental', async function (assert) {
+  });
+});
