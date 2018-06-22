a37d1799c78f7f880b676b08f355a80663e29f87
ijlee2 <ijlee2@users.noreply.github.com>
2018-06-21 23:25:52 -0500

    Added a simple component

--- ember-official-3_1_4/tests/acceptance/list-rentals-test.js
+++ ember-official-3_1_4/tests/acceptance/list-rentals-test.js
@@ -23,6 +23,8 @@ module('Acceptance | list rentals', function(hooks) {
   });
 
   test('should list available rentals.', async function (assert) {
+    await visit('/');
+    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
   });
 
   test('should filter the list of rentals by city.', async function (assert) {
---
+++ ember-official-3_1_4/tests/integration/components/rental-listing-test.js
@@ -0,0 +1,35 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import EmberObject from '@ember/object';
+import { render, click } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | rental listing', function(hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function() {
+    this.rental = EmberObject.create({
+      image: 'fake.png',
+      title: 'test-title',
+      owner: 'test-owner',
+      type: 'test-type',
+      city: 'test-city',
+      bedrooms: 3
+    });
+  });
+
+  test('should display rental details', async function(assert) {
+    await render(hbs`{{rental-listing rental=rental}}`);
+    assert.equal(this.element.querySelector('.listing h3').textContent.trim(), 'test-title', 'Title: test-title');
+    assert.equal(this.element.querySelector('.listing .owner').textContent.trim(), 'Owner: test-owner', 'Owner: test-owner');
+  });
+
+  test('should toggle wide class on click', async function(assert) {
+    await render(hbs`{{rental-listing rental=rental}}`);
+    assert.notOk(this.element.querySelector('.image.wide'), 'initially rendered small');
+    await click('.image');
+    assert.ok(this.element.querySelector('.image.wide'), 'rendered wide after click');
+    await click('.image');
+    assert.notOk(this.element.querySelector('.image.wide'), 'rendered small after second click');
+  });
+});
