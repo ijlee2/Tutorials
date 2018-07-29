1f63bf103ee1de75d2e502faf9ad643c9797d0c3
ijlee2 <ijlee2@users.noreply.github.com>
2018-07-28 21:39:51 -0500

    Working on adding CRUD operations

--- rails-official/config/routes.rb
+++ rails-official/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'welcome/index'     # Map requests to http://localhost:3000/welcome/index
+                            # to the welcome controller's index action
+
+    resources :articles
+
+    root 'welcome#index'    # Map requests to the root of the application to the
+                            # welcome controller's `index` action
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
